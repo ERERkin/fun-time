6299c2954b8211d69c336ba302bd9c9fe92e9a6b
Talaibekov Erkinbek <32312803+ERERkin@users.noreply.github.com>
2021-11-21 22:47:45 +0600

    Create project

---
+++ src/main/java/kg/erkin/FunTime/FunTimeApplication.java
@@ -0,0 +1,13 @@
+package kg.erkin.FunTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/erkin/FunTime/FunTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.erkin.FunTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
